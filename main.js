b30905223f1d0a79c6d97a78713e737a823a93e6
Frari <francescoriccardi86@gmail.com>
2019-04-23 16:51:38 +0200

    fine

--- main.js
+++ main.js
@@ -1,20 +1,21 @@
 // chiedo all'utente il suo cognome
 var cognomeUtente=prompt("Iserisci il tuo cognome");
+
 // creo array cognomi
 var listaCognomi = ['Rossi','Bianchi','Aligheri','Manzoni','Marquez'];
+
 // inserire elemento all'interno di lista
 listaCognomi.push(cognomeUtente);
 
-document.writeln(listaCognomi.sort());
-
-// ciclo while per determinare posizione dell'elemento all'interno dell'array
+// assegnazione variabile per posizione all'interno dell'array
 var a = listaCognomi.indexOf(cognomeUtente);
 
+// ciclo while per determinare posizione dell'elemento all'interno dell'array
 var i = 0;
 while(i<listaCognomi.length){
-  document.writeln(a);
   i++
 }
-// stampare a video la lista in ordine alfabetico
 
-// dire all'utente in che posizione si trova il suo cognome
+// stampa dei cognomi e della posizione
+document.getElementById('list').innerHTML=listaCognomi.sort();
+document.getElementById('position').innerHTML='Il tuo Cognome è nella posizione: ' + a;
